083000e23aaf92d07e068356c85f7fc51216c0c6
Zeitlin1 <Zeitlin1@gmail.com>
2016-09-26 20:21:15 -0400

    Add files via upload
    add solution code

--- ViewController.swift
+++ ViewController.swift
@@ -10,15 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textDisplay: UILabel!
+    
+    @IBAction func helloWorld(_ sender: AnyObject) {
+        print("Pressed Hello World button")
+        textDisplay.text = "Hello, world!"
+    }
+    
+    @IBAction func goodbyeWorld(_ sender: AnyObject) {
+        print("Pressed Goodbye World button")
+        textDisplay.text = "Goodbye, world!"
+    }
+    @IBAction func unicorn(_ sender: AnyObject) {
+        print("Pressed unicorns button")
+        textDisplay.text = "ze Unicorns"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        textDisplay.text = "Zzzz..."
+        print("SEE THIS IS CRAZY!")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-
    
 
 }
